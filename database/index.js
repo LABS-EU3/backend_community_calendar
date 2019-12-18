633b6df48d90f829ed02547250e0df8d23f44b0a
samuel <osamaimafidon@gmail.com>
2019-12-18 17:15:01 +0100

    refactor(backend): consume the connection url from dbconfig

--- database/index.js
+++ database/index.js
@@ -1,38 +1,15 @@
 const mongoose = require("mongoose");
 
-const dbConfig = require("../config/dbConfig");
+const connectionUrl = require("../config/dbConfig");
 
 module.exports = async () => {
-  let url = "";
-
-  switch (process.env.ENVIRONMENT) {
-    case "development":
-      url = dbConfig.DATABASE_URL_DEVELOPMENT;
-      break;
-
-    case "staging":
-      url = dbConfig.DATABASE_URL_STAGING;
-      break;
-
-    case "test":
-      url = dbConfig.DATABASE_URL_TEST;
-      break;
-
-    case "production":
-      url = dbConfig.DATABASE_URL_PRODUCTION;
-      break;
-
-    default:
-      url = dbConfig.DATABASE_URL_DEVELOPMENT;
-  }
-
   try {
-    await mongoose.connect(url, {
+    await mongoose.connect(connectionUrl, {
       useNewUrlParser: true,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
     });
     console.log("Connected to Database");
   } catch (e) {
-    console.log("Connection To Database Failed");
+    console.log(`Connection to database failed: ${e.message}`);
   }
 };
