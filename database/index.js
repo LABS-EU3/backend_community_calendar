9e96ee7dc1dea612b6e44915f6880564c827a95b
samuel <osamaimafidon@gmail.com>
2019-12-18 16:06:11 +0100

    refactor(backend): refactor the database setup Add a connect module in the /database folder Created a dbConfig file to store all database configurations Replaced the IIFE in app.js with a call to the dbConnection module Updatad the README.md to include the default local database connection url

--- database/index.js
+++ database/index.js
@@ -1,28 +1,30 @@
 const mongoose = require("mongoose");
 
+const dbConfig = require('../config/dbConfig');
+
 module.exports = () => {
   return new Promise((resolve) => {
     let url = "";
 
     switch (process.env.ENVIRONMENT) {
       case "development":
-        url = process.env.DATABASE_URL_DEVELOPMENT;
+        url = dbConfig.DATABASE_URL_DEVELOPMENT;
         break;
 
       case "staging":
-        url = process.env.DATABASE_URL_STAGING;
+        url = dbConfig.DATABASE_URL_STAGING;
         break;
 
       case "test":
-        url = process.env.DATABASE_URL_TEST;
+        url = dbConfig.DATABASE_URL_TEST;
         break;
 
       case "production":
-        url = process.env.DATABASE_URL_PRODUCTION;
+        url = dbConfig.DATABASE_URL_PRODUCTION;
         break;
 
       default:
-        url = process.env.DATABASE_URL_TEST;
+        url = dbConfig.DATABASE_URL_DEVELOPMENT;
     }
 
     mongoose.connect(
