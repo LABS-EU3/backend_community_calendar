e81173598d5c7188d4f3aed32ed08c9ba09c726a
samuel <osamaimafidon@gmail.com>
2019-12-18 17:14:12 +0100

    refactor(backend): modify dbconfig to return db connection url

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,8 +1,24 @@
-const config = {
-  DATABASE_URL_TEST: process.env.DATABASE_URL_TEST,
-  DATABASE_URL_STAGING: process.env.DATABASE_URL_STAGING,
-  DATABASE_URL_DEVELOPMENT: process.env.DATABASE_URL_DEVELOPMENT,
-  DATABASE_URL_PRODUCTION: process.env.DATABASE_URL_PRODUCTION,
-};
+let connectionUrl = "";
 
-module.exports = config;
+switch (process.env.NODE_ENV) {
+  case "development":
+    connectionUrl = process.env.DATABASE_URL_DEVELOPMENT;
+    break;
+
+  case "staging":
+    connectionUrl = process.env.DATABASE_URL_STAGING;
+    break;
+
+  case "test":
+    connectionUrl = process.env.DATABASE_URL_TEST;
+    break;
+
+  case "production":
+    connectionUrl = process.env.DATABASE_URL_PRODUCTION;
+    break;
+
+  default:
+    connectionUrl = process.env.DATABASE_URL_DEVELOPMENT;
+}
+
+module.exports = connectionUrl;
--- database/index.js
+++ database/index.js
@@ -1,38 +1,38 @@
 const mongoose = require("mongoose");
 
-const dbConfig = require('../config/dbConfig');
+const dbConfig = require("../config/dbConfig");
 
-module.exports = () => {
-  return new Promise((resolve) => {
-    let url = "";
+module.exports = async () => {
+  let url = "";
 
-    switch (process.env.ENVIRONMENT) {
-      case "development":
-        url = dbConfig.DATABASE_URL_DEVELOPMENT;
-        break;
+  switch (process.env.ENVIRONMENT) {
+    case "development":
+      url = dbConfig.DATABASE_URL_DEVELOPMENT;
+      break;
 
-      case "staging":
-        url = dbConfig.DATABASE_URL_STAGING;
-        break;
+    case "staging":
+      url = dbConfig.DATABASE_URL_STAGING;
+      break;
 
-      case "test":
-        url = dbConfig.DATABASE_URL_TEST;
-        break;
+    case "test":
+      url = dbConfig.DATABASE_URL_TEST;
+      break;
 
-      case "production":
-        url = dbConfig.DATABASE_URL_PRODUCTION;
-        break;
+    case "production":
+      url = dbConfig.DATABASE_URL_PRODUCTION;
+      break;
 
-      default:
-        url = dbConfig.DATABASE_URL_DEVELOPMENT;
-    }
+    default:
+      url = dbConfig.DATABASE_URL_DEVELOPMENT;
+  }
 
-    mongoose.connect(
-      url,
-      { useNewUrlParser: true, useUnifiedTopology: true },
-      () => {
-        resolve(true);
-      }
-    );
-  });
+  try {
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log("Connected to Database");
+  } catch (e) {
+    console.log("Connection To Database Failed");
+  }
 };
