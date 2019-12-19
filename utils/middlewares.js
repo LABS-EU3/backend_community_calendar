1021546d21210d78a106a5015ea28ad9a8f83375
samuel <osamaimafidon@gmail.com>
2019-12-19 11:04:07 +0100

    fix(backend): fix a bug with the morgan middleware

--- utils/middlewares.js
+++ utils/middlewares.js
@@ -2,7 +2,7 @@ const morgan = require('morgan');
 const express = require('express');
 
 module.exports = function(app) {
-  app.use(morgan);
+  app.use(morgan("combined"));
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
 };
