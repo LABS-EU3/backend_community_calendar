087d06560af79fcedaaab242e40010e2491f8f05
Martin Twum Mensah <martint.mensah@gmail.com>
2020-01-08 08:29:42 +0000

    refactor: use mongoose's insertMany method to insert events
    Update model schema to make it possible to receive other events
    Update webScraper array object keys to match schema in the db

--- resources/event/event.route.js
+++ resources/event/event.route.js
@@ -6,6 +6,11 @@ const eventController = require("./event.controller");
 router.get('/fetch-events', async (req, res, next) => {
   try {
     const events = await eventController.addScrapedEvent();
+    if (!events) {
+      res
+        .status(500)
+        .json('Events could not be inserted or they already exist');
+    }
     res
       .status(200)
       .json(events);
