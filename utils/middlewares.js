6c91cb18bd9b30d637e6bf5efb379186b90b641b
Martin Twum Mensah <martint.mensah@gmail.com>
2020-01-08 13:54:00 +0000

    chore: enable cors and helmet
    Set a whitelist of urls that are allowed by CORS in the middlewares.js file

--- utils/middlewares.js
+++ utils/middlewares.js
@@ -1,8 +1,24 @@
 const morgan = require("morgan");
 const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
 
-module.exports = function(app) {
+const CORS_ORIGIN_WHITELIST = ['http://localhost:3000'];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (CORS_ORIGIN_WHITELIST.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error(`${origin} is not allowed by CORS`));
+    }
+  },
+};
+
+module.exports = function expressMiddlewares(app) {
   app.use(morgan("combined"));
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
+  app.use(helmet());
+  app.use(cors(corsOptions));
 };
