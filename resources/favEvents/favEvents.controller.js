dc2a861874521c57777d0c53ab8f4badea1d2daf
Josemaria Nriagu <josemarianriagu25@gmail.com>
2020-01-18 21:31:16 +0100

    improvement(backend): implement delete favorite

--- resources/favEvents/favEvents.controller.js
+++ resources/favEvents/favEvents.controller.js
@@ -1,9 +1,15 @@
-const UserFav = require('./favEvents.model');
+const FavEvent = require('./favEvents.model');
 
 module.exports = {
   async saveFav(eventId, userId) {
-    const favedEvent = new UserFav({ eventId, userId });
+    const favedEvent = new FavEvent({ eventId, userId });
     await favedEvent.save();
     return favedEvent;
+  },
+  async deleteFav(savedFavId) {
+    const deletedFav = await FavEvent.findOneAndDelete({
+      _id: savedFavId,
+    });
+    return deletedFav;
   },
 };
--- resources/favEvents/favEvents.route.js
+++ resources/favEvents/favEvents.route.js
@@ -2,18 +2,35 @@ const express = require('express');
 
 const router = express.Router();
 
-const userFavController = require('./favEvents.controller');
+const favEventsController = require('./favEvents.controller');
 
 router.post('/', async (req, res) => {
   try {
     const { eventId, userId } = req.body;
-    const savedEvent = await userFavController.saveFav(eventId, userId);
+    const savedEvent = await favEventsController.saveFav(eventId, userId);
     if (!savedEvent) {
       throw new Error('Event could not be saved at this time. Please try again later');
     }
     res.status(201).json({ message: 'Favorite event saved successfully', savedEvent });
   } catch (error) {
-    res.status(500).json(error);
+    res.status(500).json(error.message);
+  }
+});
+
+router.delete('/', async (req, res) => {
+  try {
+    const { savedFavId } = req.body;
+    if (savedFavId !== undefined) {
+      const deletedFav = await favEventsController.deleteFav(savedFavId);
+      if (!deletedFav) {
+        throw new Error('Favorite event not found or could not be removed at this time. Please try again later');
+      }
+      res.status(200).json({ message: 'Event successfully removed as favorite', deletedFav });
+    } else {
+      res.status(400).json({ message: 'supply valid favorite eventId' });
+    }
+  } catch (error) {
+    res.status(500).json(error.message);
   }
 });
 
