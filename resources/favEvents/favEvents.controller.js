38dcfaca350fd7a3ae37c6fe3aabcccb54b28690
Josemaria Nriagu <josemarianriagu25@gmail.com>
2020-01-20 01:17:19 +0100

    feat(backend): implement GET saved favorites

--- resources/favEvents/favEvents.controller.js
+++ resources/favEvents/favEvents.controller.js
@@ -1,6 +1,28 @@
 const FavEvent = require('./favEvents.model');
+const Event = require('../event/event.model');
 
 module.exports = {
+  async findFav(userId) {
+    const favEventsId = await FavEvent.find({ userId }, 'eventId userId', (err, docs) => {
+      if (err) {
+        return false;
+      }
+      return docs;
+    });
+    return favEventsId;
+  },
+  async findEvents(evArr) {
+    const result = await Promise.all(
+      evArr.map((event) => Event.findOne({
+        scrapedEventId: event.eventId,
+      }, (err, doc) => {
+        if (err) return false;
+        return doc._doc;
+      })),
+    )
+      .then((modelArr) => modelArr.map((model) => model._doc));
+    return result;
+  },
   async saveFav(eventId, userId) {
     const favedEvent = new FavEvent({ eventId, userId });
     await favedEvent.save();
--- resources/favEvents/favEvents.route.js
+++ resources/favEvents/favEvents.route.js
@@ -4,6 +4,27 @@ const router = express.Router();
 
 const favEventsController = require('./favEvents.controller');
 
+router.get('/', async (req, res) => {
+  try {
+    const { u } = req.query;
+    if (typeof u === 'undefined') {
+      res.status(400).json('Please supply valid id in the query');
+    } else {
+      const favEventsId = await favEventsController.findFav(u);
+      if (!favEventsId) {
+        throw new Error('Favorite events could not be retrieved at this time. Please try again later');
+      }
+      const favEvents = await favEventsController.findEvents(favEventsId);
+      if (!favEvents) {
+        throw new Error('Favorite events could not be retrieved at this time. Please try again later');
+      }
+      res.status(200).json({ message: 'Favorite events retrieved successfully', favEvents });
+    }
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+});
+
 router.post('/', async (req, res) => {
   try {
     const { eventId, userId } = req.body;
@@ -20,14 +41,14 @@ router.post('/', async (req, res) => {
 router.delete('/', async (req, res) => {
   try {
     const { eventId, userId } = req.body;
-    if (eventId !== undefined && userId !== undefined) {
+    if (typeof eventId !== 'undefined' && typeof userId !== 'undefined') {
       const deletedFav = await favEventsController.deleteFav(eventId, userId);
       if (!deletedFav) {
         throw new Error('Favorite event not found or could not be removed at this time. Please try again later');
       }
       res.status(200).json({ message: 'Event successfully removed as favorite', deletedFav });
     } else {
-      res.status(400).json({ message: 'supply valid favorite eventId' });
+      res.status(400).json({ message: 'supply valid favorite eventId and/or userId' });
     }
   } catch (error) {
     res.status(500).json(error.message);
